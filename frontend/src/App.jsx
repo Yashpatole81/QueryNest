ce5e42088d412b14bb220b3e49cc1c2e14d9a1f3
b17user48 <b17user48@rpsconsulting.in>
2026-03-22 13:43:36 +0530

    landing page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { AppProvider } from './context/AppContext';
 import Home from './pages/Home';
+import LandingPage from './pages/LandingPage';
 
 function App() {
   return (
-    <AppProvider>
-      <Home />
-    </AppProvider>
+    <BrowserRouter>
+      <AppProvider>
+        <Routes>
+          {/* Landing page is now the root URL */}
+          <Route path="/" element={<LandingPage />} />
+          {/* Chat app lives at /app */}
+          <Route path="/app" element={<Home />} />
+          {/* Catch-all redirects to landing */}
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </AppProvider>
+    </BrowserRouter>
   );
 }
 
